6fd84370888feefb9c7be545172cfe02d0f28e74
william-song-shy <47596959+william-song-shy@users.noreply.github.com>
2019-04-20 13:32:15 +0800

    Change the variable name

--- spider.py
+++ spider.py
@@ -3,8 +3,8 @@ import re
 headers={'User_Agent':"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
 num=input("题目编号：")
 response = request.Request("http://acm.hdu.edu.cn/showproblem.php?pid={}".format(num),headers=headers)
-respons = request.urlopen(response)
-html = respons.read()
+response = request.urlopen(response)
+html = response.read()
 html = html.decode("gb2312")
 title=re.findall(r"<h1 style='color:#1A5CC8'>(.*?)</h1>",html)
 ltitle=re.findall(r"<div class=panel_title align=left>(.*?)</div>",html)
