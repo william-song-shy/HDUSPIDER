7c336a9543a93fe4ee116e20e699584ff8d7347d
william-song-shy <2560814258@qq.com>
2019-04-22 13:43:54 +0800

    Put work in class

--- spider.py
+++ spider.py
@@ -1,9 +1,20 @@
 from urllib import request
 import re
 class HDU:
-        def __init__ (self,num=1001,headers={'User_Agent':"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}):
+        def __init__ (self,num=1001):
                 self.num=num
-                self.headers=headers
+        def work (self,headers={'User_Agent':"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}):
+                response = request.Request("http://acm.hdu.edu.cn/showproblem.php?pid={}".format(self.num),headers=headers)
+                response = request.urlopen(response)
+                html = response.read()
+                html = html.decode("gb2312")
+                title=re.findall(r"<h1 style='color:#1A5CC8'>(.*?)</h1>",html)
+                ltitle=re.findall(r"<div class=panel_title align=left>(.*?)</div>",html)
+                test=re.findall(r"<div class=panel_content>([\d\D]*?)</div>",html)
+                d={}
+                for i,d[ltitle[i]] in enumerate(test):
+                        pass
+                return (title[0],d)
 if __name__=='__main__':
 	problem=HDU(input())
-	print (problem.html())
+	print (problem.work())
