ced275dce8b41ec67284f8c49717c4c7c405f936
william-song-shy <2560814258@qq.com>
2019-04-22 13:30:58 +0800

    Delete func html in HDU & work
    Before the next version, the program will not be available.

--- spider.py
+++ spider.py
@@ -4,20 +4,6 @@ class HDU:
         def __init__ (self,num=1001,headers={'User_Agent':"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}):
                 self.num=num
                 self.headers=headers
-        def html (self):
-                return work(self.num,self.headers)
-def work (num,headers):
-	response = request.Request("http://acm.hdu.edu.cn/showproblem.php?pid={}".format(num),headers=headers)
-	response = request.urlopen(response)
-	html = response.read()
-	html = html.decode("gb2312")
-	title=re.findall(r"<h1 style='color:#1A5CC8'>(.*?)</h1>",html)
-	ltitle=re.findall(r"<div class=panel_title align=left>(.*?)</div>",html)
-	test=re.findall(r"<div class=panel_content>([\d\D]*?)</div>",html)
-	d={}
-	for i,d[ltitle[i]] in enumerate(test):
-		pass
-	return (title[0],d)
 if __name__=='__main__':
 	problem=HDU(input())
 	print (problem.html())
