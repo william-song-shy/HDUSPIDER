4b9ec45ec302e53f9ed1421adc526385bd1c5d07
william-song-shy <2560814258@qq.com>
2019-04-23 21:24:44 +0800

    Add class HDUError
    raise HDUError when not found question

--- spider.py
+++ spider.py
@@ -1,5 +1,7 @@
 from urllib import request
 import re
+class HDUError (Exception):
+	pass
 class HDU:
         def __init__ (self,num=1001):
                 self.num=num
@@ -7,11 +9,11 @@ class HDU:
                 response = request.Request("http://acm.hdu.edu.cn/showproblem.php?pid={}".format(self.num),headers=headers)
                 response = request.urlopen(response)
                 if response.status==404:
-                        raise NotFound ('Not found error of {}'.format(self.num))
+                        raise HDUError ('Not found error of {}'.format(self.num))
                 html = response.read()
                 html = html.decode("gb2312")
                 if  not re.findall('No such problem - ',html)==[]:
-                        raise Exception ('Not found error of {}'.format(self.num))
+                        raise HDUError ('Not found error of {}'.format(self.num))
                 title=re.findall(r"<h1 style='color:#1A5CC8'>(.*?)</h1>",html)
                 ltitle=re.findall(r"<div class=panel_title align=left>(.*?)</div>",html)
                 test=re.findall(r"<div class=panel_content>([\d\D]*?)</div>",html)
